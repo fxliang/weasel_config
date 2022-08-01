3f6162e430e0a3c39d3f9d524abfa78d3347bba2
liangfx <fx.liang@outlook.com>
2022-08-01 16:52:25 +0800

    增加换行filter wubi86.custom.yaml增加和wubi091一样的pattern

--- rime.lua
+++ rime.lua
@@ -13,16 +13,32 @@ function single_char_first_filter(input)
 	end
 end
 
+-- 在词典里面候选支持\r分割分行
+-- 方案patch中要添加以下两行
+--  engine/filters/+:
+--  - lua_filter@Multiline_filter
+function Multiline_filter(input)
+	for cand in input:iter() do
+		if cand.text:match("\\r") then
+			local nt = string.gsub(cand.text, "\\r", utf8.char(13))
+			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
+			yield(cnd)
+		else
+			yield(cand)
+		end
+	end
+end
+
 function test(input, seg)
 	local cand = "                    后宮詞\r              白居易【唐代】\r泪湿罗巾梦不成，夜深前殿按歌声。\r红颜未老恩先断，斜倚薰笼坐到明。"
 	if string.match(input, "^/bjy$") then
 		local pth = package.path
-		local ss = string.gsub(pth, ";", "\r")
+		--local ss = string.gsub(pth, ";", "\r")
 		yield(Candidate("time", seg.start, seg._end, cand, ""))
 		yield(Candidate("time", seg.start, seg._end, utf8.char(0x30000), ""))
 		yield(Candidate("time", seg.start, seg._end, utf8.char(0x30001), ""))
 		yield(Candidate("time", seg.start, seg._end, utf8.char(0x30002), ""))
-		yield(Candidate("test", seg.start, seg._end, ss, ""))
+		--yield(Candidate("test", seg.start, seg._end, ss, ""))
 	end
 end
 
