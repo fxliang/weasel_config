5907782a20253c1dd6c32db8386fbc33a9f9e8c5
liangfx <fx.liang@outlook.com>
2023-03-24 19:44:55 +0800

    multi lines function simplification

--- lua/basefunctions.lua
+++ lua/basefunctions.lua
@@ -12,30 +12,40 @@ function single_char_first_filter(input)
 	end
 end
 
--- 在词典里面候选支持\r, \r\n, \n分割分行
+-- global lua help tips
+function Lua_Tips(input,seg, env)
+	if input:match("^/$") then 
+		local segment = env.engine.context.composition:back()
+		segment.prompt = "~? [获得输入帮助]"
+		return
+	end
+	if input:match("^/%?$") then
+		local segment = env.engine.context.composition:back()
+		segment.prompt = "Lua快捷输入帮助"
+		local emoji_tips = Emoji_Tips()
+		local daxie_tips = Daxie_Tips()
+		local unicode_tips = Unicode_Tips()
+		local date_tips = Date_Tips()
+		yield(Candidate("tips", seg._start, seg._end, emoji_tips, ""))
+		yield(Candidate("tips", seg._start, seg._end, date_tips, ""))
+		yield(Candidate("tips", seg._start, seg._end, unicode_tips, ""))
+		yield(Candidate("tips", seg._start, seg._end, daxie_tips, ""))
+	end
+end
+-- 在词典里面候选支持\r, \r\n, \n, <br>分割分行
+-- &nbsp 表示空格
 -- 方案patch中要添加以下两行
 --  engine/filters/+:
 --  - lua_filter@Multiline_filter
 function Multiline_filter(input)
 	for cand in input:iter() do
 		local nt = cand.text
-		if nt:match("&nbsp") then
-			nt = nt:gsub("&nbsp", " ")	
-		end
-		if nt:match("\\r\\n") then
-			nt = string.gsub(nt, "\\r\\n", "\r")
-			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
-			yield(cnd)
-		elseif nt:match("\\r") then
-			nt = string.gsub(nt, "\\r", "\r")
-			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
-			yield(cnd)
-		elseif nt:match("\\n") then
-			nt = string.gsub(nt, "\\n", "\r")
-			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
-			yield(cnd)
-		elseif nt:match("<br>") then
-			nt = string.gsub(nt, "<br>", "\r")
+		if nt:match("\\r\\n") or nt:match("\\r") or nt:match("\\n") or nt:match("<br>") or nt:match("&nbsp") then
+			nt = nt:gsub("&nbsp", " ")
+			nt = nt:gsub("\\r\\n", "\r")
+			nt = nt:gsub("\\r", "\r")
+			nt = nt:gsub("\\n", "\r")
+			nt = nt:gsub("<br>", "\r")
 			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
 			yield(cnd)
 		else
--- rime.lua
+++ rime.lua
@@ -3,27 +3,6 @@ require("unicode")
 require("number")
 require("emoji")
 require("basefunctions")
-
--- global lua help tips
-function Lua_Tips(input,seg, env)
-	if input:match("^/$") then 
-		local segment = env.engine.context.composition:back()
-		segment.prompt = "~? [获得输入帮助]"
-		return
-	end
-	if input:match("^/%?$") then
-		local segment = env.engine.context.composition:back()
-		segment.prompt = "Lua快捷输入帮助"
-		local emoji_tips = Emoji_Tips()
-		local daxie_tips = Daxie_Tips()
-		local unicode_tips = Unicode_Tips()
-		local date_tips = Date_Tips()
-		yield(Candidate("tips", seg._start, seg._end, emoji_tips, ""))
-		yield(Candidate("tips", seg._start, seg._end, date_tips, ""))
-		yield(Candidate("tips", seg._start, seg._end, unicode_tips, ""))
-		yield(Candidate("tips", seg._start, seg._end, daxie_tips, ""))
-	end
-end
 --must after function definitions
 local english = require("english")()
 english_processor = english.processor
