55dea7aec578fbd1623fce62cce0fcba70cbe87b
liangfx <fx.liang@outlook.com>
2023-04-07 21:43:07 +0800

    fix multi line

--- lua/basefunctions.lua
+++ lua/basefunctions.lua
@@ -40,8 +40,12 @@ end
 function Multiline_filter(input)
 	for cand in input:iter() do
 		local nt = cand.text
-		if rime_api.regex_match(nt, ".*(\\n|\\r|<br>).*") then
-			rime_api.regex_replace(nt, "\\r\\n|\\r|\\n|<br>", "\r")
+		if nt:match("\\r\\n") or nt:match("\\r") or nt:match("\\n") or nt:match("<br>") or nt:match("&nbsp") then
+			nt = nt:gsub("&nbsp", " ")
+			nt = nt:gsub("\\r\\n", "\r")
+			nt = nt:gsub("\\r", "\r")
+			nt = nt:gsub("\\n", "\r")
+			nt = nt:gsub("<br>", "\r")
 			local cnd = Candidate("", cand.start, cand._end, nt, cand.comment)
 			yield(cnd)
 		else
