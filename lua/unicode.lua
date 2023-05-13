cf2d99dba2b22c13218290c89fe5b3ed2f25eb27
liangfx <fx.liang@outlook.com>
2023-05-13 18:15:07 +0800

    unicode.lua base.yaml feat: Unicode range input styles.yaml feat: inline_preedit in app_options styles.yaml: remove useless settings wubi091.dict.yaml: add a word 从机

--- lua/unicode.lua
+++ lua/unicode.lua
@@ -1,6 +1,6 @@
 -- Unicode quick input
 function Unicode_Translator(input, seg, env)
-	if not (input:match("^U") or input:match("^/uc")) then return end
+	if not (input:match("^U") or input:match("^/uc") or input:match("^/ur")) then return end
 	local segment = env.engine.context.composition:back()
 	if input:match("^U$") or input:match("^/uc$") then
 		segment.prompt = "输入[0-9a-fA-F],以[^0-9a-fA-F]分割两个字符，Unicode"
@@ -24,6 +24,21 @@ function Unicode_Translator(input, seg, env)
 			local cc = Candidate("Unicode", seg.start, seg._end, cand, cmt)
 			yield(cc)
 		end
+	elseif input:match("^/ur%x+-%x+") then
+		local st_ = input:sub(4)
+		st_ = string.match(st_, "^%x+")
+		local end_ = input:match("-%x+")
+		end_ = string.sub(end_, 2)
+		local st__ = tonumber(st_, 16)
+		local end__ = tonumber(end_, 16)
+		if end__ < st__ or end__ > 0x10ffff or st__ > 0x10ffff then return end
+		local txt = ""
+		for c = st__, end__ do 
+			txt = txt .. utf8.char(c)
+		end
+		local cmt = "\\u+" .. st_:upper() .. " ~ " .. "\\u+" .. end_:upper()
+		local cc = Candidate("Unicode", seg.start, seg._end, txt, cmt)
+		yield(cc)
 	end
 end
 
